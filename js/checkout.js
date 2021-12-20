3233128a88bacc5641fff0a77ae5b892cb24a605
joselesquinas <tecnicjoncs@gmail.com>
2021-12-20 18:13:20 +0100

    en ejercicio 6

--- js/checkout.js
+++ js/checkout.js
@@ -1,12 +1,20 @@
+const form = document.querySelector('.form');
+const inputs = document.querySelectorAll('.form input');
+
 // Get the input fields
+var nom = document.querySelectorAll('.name');
+
 var password = document.querySelector(".password");
+
 var phone = document.querySelector('.phone');
-var name = document.querySelector('.name');
 
 // Get the error elements
-var errorPassword = document.getElementById("errorPassword");
 var errorName = document.getElementById('errorName');
+
+var errorPassword = document.getElementById("errorPassword");
+
 var errorPhone = document.getElementById('errorPhone');
+
 /* TODO 
 - Tots els camps són obligatoris.
 
@@ -21,19 +29,58 @@ var errorPhone = document.getElementById('errorPhone');
 - L'email ha de tenir format d'email.
 */
 
-// Exercise 6
+const expresiones = {
+   nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+   apellido: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+   correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+   password: /^.{4,12}$/, // 4 a 12 digitos.
+   direccion: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+   telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+};
 
+
+const validateCampos = (e) => {
+
+   switch (e.target.name) {
+      case "fname":
+         if(expresiones.nombre.test()){
+
+         } else {
+
+         };
+      break;
+      case "lname":
+         
+      break;
+      case "email":
+         
+      break;
+      case "Password":
+
+      break;
+      case "address":
+            
+      break;
+      case "phone":
+            
+      break;     
+
+   }
+
+};
+
+
+inputs.forEach((input) => {
+   input.addEventListener('keyup', validateCampos);
+   input.addEventListener('blur', validateCampos);
+});
+
+
+// Exercise 6
 function validate() {
     // Validate fields entered by the user: name, phone, password, and email
     // First Name, 
-  
-    // errorPhone.style.display = 'block';
-    errorPhone.style.visibility = 'visible';
-
-      // display: none;
-      // display: block;
-      
-
-
-
+     
+   
 }
+
